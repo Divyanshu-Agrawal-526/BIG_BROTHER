da513e937fc29a65b56e5628bff604f80c29305f
Divyanshu-Agrawal-526 <118790383+Divyanshu-Agrawal-526@users.noreply.github.com>
2022-11-27 07:47:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from st_functions import st_button, load_css
 from PIL import Image
+import mysql.connector
 
 load_css()
 
@@ -24,3 +25,22 @@ st_button('github', 'https://github.com/Divyanshu-Agrawal-526', 'Follow Divyansh
 st_button('github', 'https://github.com/N4SK4R', 'Follow Aditya Naskar on Github', icon_size)
 st_button('github', 'https://github.com/akshat-sj/big-brother', 'Follow BIG BROTHER SOURCE CODE on Github', icon_size)
 st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+@st.experimental_singleton
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from mytable;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
