36e5a9fc4e8fce1dd0afc46fb30b18f41206d8ed
Divyanshu-Agrawal-526 <118790383+Divyanshu-Agrawal-526@users.noreply.github.com>
2022-11-27 09:42:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,13 @@ col2.image(Image.open('dp.jpeg'))
 
 st.header('BIG BROTHER')
 icon_size = 20
-st.header(st_button('webcam','SACRED EYE',icon_size)
+st.header('SACRED EYE')
           
 
 
 st.info('Developed by Akshat Singh Jaiswal, Vyoman Jain, Aditya Naskar and Divyanshu Agrawal')
 
-
+st_button('webcam',  'SACRED EYE', icon_size)
 st_button('github', 'https://github.com/akshat-sj', 'Follow Akshat Singh Jaiswal on Github', icon_size)
 st_button('github', 'https://github.com/VyoJ', 'Follow Vyoman Jain on Github', icon_size)
 st_button('github', 'https://github.com/Divyanshu-Agrawal-526', 'Follow Divyanshu Agrawal on Github', icon_size)
